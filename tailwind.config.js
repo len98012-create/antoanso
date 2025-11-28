bd7714ffd0662bb6b6c7cabfea6642bd00a0b027
len98012-create <len98012@gmail.com>
2025-11-28 21:59:32 +0700

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // bật chế độ dark mode theo class
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 
